0f0b7c34b88f82f70a1b365acd04d6e591e1d938
李满满 <limanman@corp.netease.com>
2020-11-25 15:27:03 +0800

    :underage:

--- namekox_core/core/generator.py
+++ namekox_core/core/generator.py
@@ -5,8 +5,14 @@
 
 import os
 import uuid
+import hashlib
+
+
+def generator_md5(data):
+    md5 = hashlib.md5()
+    md5.update(data)
+    return md5.hexdigest()
 
 
 def generator_uuid(length=16):
     return str(uuid.UUID(bytes=os.urandom(length), version=4))
-
