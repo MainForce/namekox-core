e30a2250af73ca73636073fa3038c33dbf001e29
李满满 <limanman@corp.netease.com>
2021-02-02 10:54:29 +0800

    :underage:

--- namekox_core/core/service/context.py
+++ namekox_core/core/service/context.py
@@ -8,53 +8,26 @@ from namekox_core.core.generator import generator_uuid
 
 class BaseContext(object):
     _call_id = None
-    _call_id_stack = None
-    _origin_call_id = None
-    _immediate_call_id = None
-    _parent_call_id_stack = None
+    _parent_call_id = None
 
     def __init__(self, context=None):
         self.context = context or {}
 
     @property
-    def data(self):
-        data = self.context.copy()
-        data['call_id_stack'] = self.call_id_stack
-        return data
-
-    @property
     def call_id(self):
-        if self._call_id is None:
-            self._call_id = generator_uuid()
+        self._call_id = self._call_id or generator_uuid()
         return self._call_id
 
     @property
-    def call_id_stack(self):
-        if self._call_id_stack is None:
-            self._call_id_stack = []
-            self._call_id_stack.extend(self.parent_call_id_stack)
-            self._call_id_stack.append(self.call_id)
-        return self._call_id_stack
+    def parent_call_id(self):
+        self._parent_call_id = self._parent_call_id or self.context.get('call_id', None)
+        return self._parent_call_id
 
     @property
-    def origin_call_id(self):
-        if self._origin_call_id is None:
-            self._origin_call_id = self.call_id_stack[0]
-        return self._origin_call_id
-
-    @property
-    def immediate_call_id(self):
-        if self._immediate_call_id is None:
-            stack = self.call_id_stack
-            stack_length = len(stack)
-            self._immediate_call_id = stack[0] if stack_length > 1 else None
-        return self._immediate_call_id
-
-    @property
-    def parent_call_id_stack(self):
-        if self._parent_call_id_stack is None:
-            self._parent_call_id_stack = self.context.pop('call_id_stack', [])
-        return self._parent_call_id_stack
+    def data(self):
+        data = self.context.copy()
+        data.update({'call_id': self.call_id, 'parent_call_id': self.parent_call_id})
+        return data
 
 
 class WorkerContext(BaseContext):
