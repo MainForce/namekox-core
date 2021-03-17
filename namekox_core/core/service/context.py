a5fa5bb979a1bafec3ec34cae24f7e3f1f19103e
李满满 <limanman@corp.netease.com>
2021-03-17 15:19:27 +0800

    :underage:

--- namekox_core/core/service/context.py
+++ namekox_core/core/service/context.py
@@ -5,20 +5,37 @@
 from __future__ import unicode_literals
 
 
+from logging import getLogger
+from namekox_core.constants import SERVICE_CONFIG_KEY
 from namekox_core.core.generator import generator_uuid
+from namekox_core.core.loaders import import_dotpath_class
 
 
-class BaseContext(object):
+logger = getLogger(__name__)
+
+
+class WorkerContext(object):
     _call_id = None
+    _origin_id = None
     _parent_call_id = None
 
-    def __init__(self, context=None):
-        self.context = context or {}
+    def __init__(self, service, entrypoint, args=None, kwargs=None, context=None):
+        self.args = args or ()
+        self.service = service
+        self.context = context
+        self.entrypoint = entrypoint
+        self.kwargs = kwargs or {}
+        self.gen_callid = get_trace_id_func(entrypoint.container.config)
 
     @property
     def call_id(self):
-        self._call_id = self._call_id or generator_uuid()
+        self._call_id = self._call_id or self.gen_callid()
         return self._call_id
+
+    @property
+    def origin_id(self):
+        self._origin_id = self.context.get('origin_id', None) or self.call_id
+        return self._origin_id
 
     @property
     def parent_call_id(self):
@@ -28,14 +45,16 @@ class BaseContext(object):
     @property
     def data(self):
         data = self.context.copy()
-        data.update({'call_id': self.call_id, 'parent_call_id': self.parent_call_id})
+        data.update({'origin_id': self.origin_id,
+                     'call_id': self.call_id,
+                     'parent_call_id': self.parent_call_id
+                     })
         return data
 
 
-class WorkerContext(BaseContext):
-    def __init__(self, service, entrypoint, args=None, kwargs=None, context=None):
-        self.args = args or ()
-        self.service = service
-        self.kwargs = kwargs or {}
-        self.entrypoint = entrypoint
-        super(WorkerContext, self).__init__(context)
+def get_trace_id_func(config):
+    fun_path = config.get(SERVICE_CONFIG_KEY, {}).get('trace_id_func', '')
+    err, fun = import_dotpath_class(fun_path, generator_uuid)
+    msg = 'load trace id func from {} failed, {}'.format(fun_path, err)
+    err and logger.warning(msg)
+    return fun
